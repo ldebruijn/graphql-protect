bf48200b85597bccf8f209f0627102d160f0497f
ldebruijn <ldebruijn@bol.com>
2023-09-24 20:30:34 +0200

    feat(field-suggestion): Fix test

--- internal/business/block_field_suggestions/block_field_suggestions_test.go
+++ internal/business/block_field_suggestions/block_field_suggestions_test.go
@@ -120,7 +120,12 @@ func TestProcessBody(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := ProcessBody(tt.args.payload); !reflect.DeepEqual(got, tt.want) {
+			b := NewBlockFieldSuggestionsHandler(Config{
+				Enabled: true,
+				Mask:    "[redacted]",
+			})
+
+			if got := b.ProcessBody(tt.args.payload); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("ProcessBody() = %v, want %v", got, tt.want)
 			}
 		})
