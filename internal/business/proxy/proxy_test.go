40939e7d694eae9329363391479ee06025aadd75
Lars de Bruijn <9264036+ldebruijn@users.noreply.github.com>
2024-01-11 22:05:45 +0100

    fix(field-suggestions): Fix block-field-suggestions rule throwing Bod… (#19)
    Fix bug on Block Field suggestions rule.
    
    We found that the block field suggestion rule broke when the response
    body did not contain the valid JSON (i.e. when proxying GraphiQL).
    
    This MR makes sure to read the response body as plain bytes, try to
    determine whether it can and should be modified and if that fails, set
    the body back to the original bytes so that it can be read again later.
    
    Co-authored-by: ldebruijn <ldebruijn@bol.com>

---
+++ internal/business/proxy/proxy_test.go
@@ -0,0 +1,144 @@
+package proxy
+
+import (
+	"github.com/ldebruijn/go-graphql-armor/internal/business/block_field_suggestions"
+	"github.com/stretchr/testify/assert"
+	"io"
+	"net/http"
+	"strings"
+	"testing"
+)
+
+func Test_modifyResponse(t *testing.T) {
+	type args struct {
+		blockFieldSuggestions *block_field_suggestions.BlockFieldSuggestionsHandler
+		response              *http.Response
+	}
+	tests := []struct {
+		name string
+		args args
+		want func(res *http.Response)
+	}{
+		{
+			name: "nothing if disabled",
+			args: args{
+				blockFieldSuggestions: func() *block_field_suggestions.BlockFieldSuggestionsHandler {
+					return block_field_suggestions.NewBlockFieldSuggestionsHandler(block_field_suggestions.Config{
+						Enabled: false,
+					})
+				}(),
+				response: func() *http.Response {
+					return &http.Response{
+						Status:        "200",
+						StatusCode:    200,
+						Body:          io.NopCloser(strings.NewReader("this is not valid json")),
+						Proto:         "HTTP/1.1",
+						ProtoMajor:    1,
+						ProtoMinor:    1,
+						ContentLength: 0,
+						Header:        map[string][]string{},
+					}
+				}(),
+			},
+			want: func(res *http.Response) {
+				body, _ := io.ReadAll(res.Body)
+				assert.Equal(t, 200, res.StatusCode)
+				assert.Equal(t, "200", res.Status)
+				assert.Equal(t, "this is not valid json", string(body))
+			},
+		},
+		{
+			name: "handles non-json gracefully",
+			args: args{
+				blockFieldSuggestions: func() *block_field_suggestions.BlockFieldSuggestionsHandler {
+					return block_field_suggestions.NewBlockFieldSuggestionsHandler(block_field_suggestions.Config{
+						Enabled: true,
+					})
+				}(),
+				response: func() *http.Response {
+					return &http.Response{
+						Status:        "200",
+						StatusCode:    200,
+						Body:          io.NopCloser(strings.NewReader("this is not valid json")),
+						Proto:         "HTTP/1.1",
+						ProtoMajor:    1,
+						ProtoMinor:    1,
+						ContentLength: 0,
+						Header:        map[string][]string{},
+					}
+				}(),
+			},
+			want: func(res *http.Response) {
+				body, _ := io.ReadAll(res.Body)
+				assert.Equal(t, 200, res.StatusCode)
+				assert.Equal(t, "200", res.Status)
+				assert.Equal(t, "this is not valid json", string(body))
+			},
+		},
+		{
+			name: "handles invalid-json gracefully",
+			args: args{
+				blockFieldSuggestions: func() *block_field_suggestions.BlockFieldSuggestionsHandler {
+					return block_field_suggestions.NewBlockFieldSuggestionsHandler(block_field_suggestions.Config{
+						Enabled: true,
+					})
+				}(),
+				response: func() *http.Response {
+					return &http.Response{
+						Status:        "200",
+						StatusCode:    200,
+						Body:          io.NopCloser(strings.NewReader("{ \"this\": \" is not valid json }")),
+						Proto:         "HTTP/1.1",
+						ProtoMajor:    1,
+						ProtoMinor:    1,
+						ContentLength: 0,
+						Header:        map[string][]string{},
+					}
+				}(),
+			},
+			want: func(res *http.Response) {
+				body, _ := io.ReadAll(res.Body)
+				assert.Equal(t, 200, res.StatusCode)
+				assert.Equal(t, "200", res.Status)
+				assert.Equal(t, "{ \"this\": \" is not valid json }", string(body))
+			},
+		},
+		{
+			name: "handles json gracefully",
+			args: args{
+				blockFieldSuggestions: func() *block_field_suggestions.BlockFieldSuggestionsHandler {
+					return block_field_suggestions.NewBlockFieldSuggestionsHandler(block_field_suggestions.Config{
+						Enabled: true,
+						Mask:    "[masked]",
+					})
+				}(),
+				response: func() *http.Response {
+					return &http.Response{
+						Status:        "200",
+						StatusCode:    200,
+						Body:          io.NopCloser(strings.NewReader("{ \"errors\": [{\"message\": \"Did you mean \"}] }")),
+						Proto:         "HTTP/1.1",
+						ProtoMajor:    1,
+						ProtoMinor:    1,
+						ContentLength: 0,
+						Header:        map[string][]string{},
+					}
+				}(),
+			},
+			want: func(res *http.Response) {
+				body, _ := io.ReadAll(res.Body)
+				assert.Equal(t, 200, res.StatusCode)
+				assert.Equal(t, "200", res.Status)
+				assert.Equal(t, "{\"errors\":[{\"message\":\"[masked]\"}]}", string(body))
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := modifyResponse(tt.args.blockFieldSuggestions)
+
+			_ = result(tt.args.response)
+			tt.want(tt.args.response)
+		})
+	}
+}
