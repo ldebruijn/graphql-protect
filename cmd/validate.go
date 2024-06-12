da2c52a81c4878785da10085f587e6b4e9f30c8f
Rick Bijkerk <rickbijkerk@bol.com>
2024-06-12 11:16:08 +0200

    dont fetch manifests from an external bucket but use what we already have locally to validate it against a given supergraph

--- cmd/validate.go
+++ cmd/validate.go
@@ -17,36 +17,36 @@ import (
 var ErrValidationErrorsFound = errors.New("errors found during validation")
 
 func validate(log *slog.Logger, cfg *config.Config, _ chan os.Signal) error {
-	if !cfg.PersistedOperations.Enabled {
-		err := fmt.Errorf("persisted operations must be enabled to run validate")
+	// We need a store defined to have files to validate
+	if cfg.PersistedOperations.Store == "" {
+		err := fmt.Errorf("store must be defined to have files to validate")
 		log.Error("Error running validations", "err", err)
 		return err
 	}
 
-	remoteLoader, err := persistedoperations.RemoteLoaderFromConfig(cfg.PersistedOperations, log)
-	if err != nil && !errors.Is(err, persistedoperations.ErrNoRemoteLoaderSpecified) {
-		log.Warn("Error initializing remote loader", "err", err)
-	}
-
-	po, err := persistedoperations.NewPersistedOperations(log, cfg.PersistedOperations, persistedoperations.NewLocalDirLoader(cfg.PersistedOperations, log), remoteLoader)
+	// Load the persisted operations from the local dir into memory
+	persistedOperations, err := persistedoperations.NewPersistedOperations(log, cfg.PersistedOperations, persistedoperations.NewLocalDirLoader(cfg.PersistedOperations, log), nil)
 	if err != nil {
 		log.Error("Error initializing Persisted Operations", "err", err)
 		return nil
 	}
 
+	// Build up the schema
 	schemaProvider, err := schema.NewSchema(cfg.Schema, log)
 	if err != nil {
 		log.Error("Error initializing schema", "err", err)
 		return nil
 	}
 
-	protectChain, err := protect.NewGraphQLProtect(log, cfg, po, schemaProvider, nil)
+	// Validate if the operations in the manifests adhere to our 'rules' (e.g. max depth/aliases/..)
+	protectChain, err := protect.NewGraphQLProtect(log, cfg, persistedOperations, schemaProvider, nil)
 	if err != nil {
 		log.Error("Error initializing GraphQL Protect", "err", err)
 		return err
 	}
 
-	errs := po.Validate(protectChain.ValidateQuery)
+	// Validate if the fields that are defined in the operation exist in our schema (this protects us from clients moving to pro before the data is there)
+	errs := persistedOperations.Validate(protectChain.ValidateQuery)
 	if len(errs) > 0 {
 		log.Warn("Errors found during validation of operations")
 		formatErrors(errs)
