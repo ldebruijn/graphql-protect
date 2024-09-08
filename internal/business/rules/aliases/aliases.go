95d7e664a162df692d22ad81df13aaa60133145e
Lars de Bruijn <9264036+ldebruijn@users.noreply.github.com>
2024-09-08 15:02:14 +0200

    feat(config): Add defaults for aliases

--- internal/business/rules/aliases/aliases.go
+++ internal/business/rules/aliases/aliases.go
@@ -23,6 +23,14 @@ type Config struct {
 	RejectOnFailure bool `conf:"default:true" yaml:"reject_on_failure"`
 }
 
+func DefaultConfig() Config {
+	return Config{
+		Enabled:         true,
+		Max:             15,
+		RejectOnFailure: true,
+	}
+}
+
 func init() {
 	prometheus.MustRegister(resultCounter)
 }
