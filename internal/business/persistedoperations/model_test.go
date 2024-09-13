9c2ebc29fa3cb2cbc5067250b2b68db37657677f
Rick Bijkerk <rickbijkerk@bol.com>
2024-09-13 16:36:23 +0200

    fix tests

--- internal/business/persistedoperations/model_test.go
+++ internal/business/persistedoperations/model_test.go
@@ -1,7 +1,6 @@
 package persistedoperations
 
 import (
-	"errors"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -13,7 +12,7 @@ func TestNewPersistedOperation(t *testing.T) {
 	tests := []struct {
 		name string
 		args args
-		want PersistedOperation
+		want string
 		err  error
 	}{
 		{
@@ -21,90 +20,63 @@ func TestNewPersistedOperation(t *testing.T) {
 			args: args{
 				operation: "query ProductQuery{ product(id: 1) { id title as } }",
 			},
-			want: PersistedOperation{
-				Operation: "query ProductQuery{ product(id: 1) { id title as } }",
-				Name:      "ProductQuery",
-			},
+			want: "ProductQuery",
 		},
 		{
 			name: "extracts operation from mutation",
 			args: args{
 				operation: "mutation ProductQuery{ product(id: 1) { id title as } }",
 			},
-			want: PersistedOperation{
-				Operation: "mutation ProductQuery{ product(id: 1) { id title as } }",
-				Name:      "ProductQuery",
-			},
+			want: "ProductQuery",
 		},
 		{
 			name: "no operation name when not present",
 			args: args{
 				operation: "mutation { product(id: 1) { id title as } }",
 			},
-			want: PersistedOperation{
-				Operation: "mutation { product(id: 1) { id title as } }",
-				Name:      "",
-			},
+			want: "",
 		},
 		{
 			name: "no operation name when no space between type and bracket",
 			args: args{
 				operation: "mutation{ product(id: 1) { id title as } }",
 			},
-			want: PersistedOperation{
-				Operation: "mutation{ product(id: 1) { id title as } }",
-			},
+			want: "",
 		},
 		{
 			name: "excludes operation arguments",
 			args: args{
 				operation: "query Foobar($some: Int, $value: String){ product(id: 1) { id title as } }",
 			},
-			want: PersistedOperation{
-				Operation: "query Foobar($some: Int, $value: String){ product(id: 1) { id title as } }",
-				Name:      "Foobar",
-			},
+			want: "Foobar",
 		},
 		{
 			name: "no weird stuff when getting a completely malformed string",
 			args: args{
 				operation: "",
 			},
-			want: PersistedOperation{
-				Operation: "",
-				Name:      "",
-			},
+			want: "",
 		},
 		{
 			name: "handles white space around operation name",
 			args: args{
 				operation: "query Foobar ($some: Int, $value: String){ product(id: 1) { id title as } }",
 			},
-			want: PersistedOperation{
-				Operation: "query Foobar ($some: Int, $value: String){ product(id: 1) { id title as } }",
-				Name:      "Foobar",
-			},
+			want: "Foobar",
 		},
 		{
 			name: "error is thrown on non parseable queries",
 			args: args{
 				operation: "invalidQueryString",
 			},
-			want: PersistedOperation{
-				Operation: "invalidQueryString",
-				Name:      "",
-			},
-			err: errors.New("no operation name match found for query/mutation invalidQueryString"),
+			want: "",
 		},
 		{
 			name: "Can deal with fragments inside a query",
 			args: args{
 				operation: "fragment BaseItem on MenuItem { action { menuItemActionType url } id imageUrl level measurement { clickedMenuItem } title } query MenuItems($country: String!, $id: String, $language: String!, $levels: String) { menuItems(country: $country, language: $language, id: $id, levels: $levels) { children { children { ...BaseItem } ...BaseItem } imageHeaderUrl level ...BaseItem } }",
 			},
-			want: PersistedOperation{
-				Operation: "fragment BaseItem on MenuItem { action { menuItemActionType url } id imageUrl level measurement { clickedMenuItem } title } query MenuItems($country: String!, $id: String, $language: String!, $levels: String) { menuItems(country: $country, language: $language, id: $id, levels: $levels) { children { children { ...BaseItem } ...BaseItem } imageHeaderUrl level ...BaseItem } }",
-				Name:      "MenuItems",
-			},
+			want: "MenuItems",
 		},
 	}
 	for _, tt := range tests {
